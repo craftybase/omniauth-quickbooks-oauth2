6bc6fa9f228ca56fc73bbb03430a287e33fca140
Lee Humes <leehumes@Lees-MacBook-Pro-2.local>
2024-11-25 11:49:40 +1100

    Align fork with local repo version

--- lib/omniauth/strategies/quickbooks_oauth2.rb
+++ lib/omniauth/strategies/quickbooks_oauth2.rb
@@ -7,27 +7,38 @@ module OmniAuth
 
       option(
         :client_options,
-        {
-          site: 'https://appcenter.intuit.com/connect/oauth2',
-          authorize_url: 'https://appcenter.intuit.com/connect/oauth2',
-          token_url: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-        },
+        site: 'https://appcenter.intuit.com/connect/oauth2',
+        authorize_url: 'https://appcenter.intuit.com/connect/oauth2',
+        token_url: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer'
       )
 
-      uid { request.params['realmId'] }
+      uid { realm_id }
 
       info do
-        raw_info
+        {
+          name: "#{raw_info[:given_name]} #{raw_info[:family_name]}",
+          email: raw_info[:email_verified] ? raw_info[:email] : nil,
+          shop_name: extra_info[:company_info][:company_name],
+          country: extra_info[:company_info][:country],
+          first_name: raw_info[:given_name],
+          last_name: raw_info[:family_name],
+          phone: raw_info[:phone_number_verified] ? raw_info[:phoneNumber] : nil,
+        }
       end
 
       extra do
-        { raw_info: raw_info }
+        {
+          raw_info: raw_info,
+          extra_info: extra_info,
+        }
       end
 
       def raw_info
-        @raw_info ||= options.scope.split(/\s+/).include?('openid') ?
-          JSON.parse(access_token.get("https://#{accounts_domain}/v1/openid_connect/userinfo").body) :
-          {}
+        @raw_info ||= scope?("openid") ? api_call(openid_endpoint) : {}
+      end
+
+      def extra_info
+        @extra_info ||= scope?("com.intuit.quickbooks.accounting") ? api_call(company_info_endpoint) : {}
       end
 
       def callback_url
@@ -36,8 +47,40 @@ module OmniAuth
 
       private
 
+      def scope?(scope)
+        options.scope.split(/\s+/).include?(scope)
+      end
+
       def accounts_domain
-        false == options.sandbox ? 'accounts.platform.intuit.com' : 'sandbox-accounts.platform.intuit.com'
+        "#{sandbox_prefix}accounts.platform.intuit.com"
+      end
+
+      def api_domain
+        "#{sandbox_prefix}quickbooks.api.intuit.com"
+      end
+
+      def sandbox_prefix
+        options.sandbox ? "sandbox-" : ""
+      end
+
+      def realm_id
+        @realm_id ||= request.params["realmId"]
+      end
+
+      def openid_endpoint
+        "https://#{accounts_domain}/v1/openid_connect/userinfo"
+      end
+
+      def company_info_endpoint
+        "https://#{api_domain}/v3/company/#{realm_id}/companyinfo/#{realm_id}"
+      end
+
+      def api_call(url)
+        deep_symbolize(access_token.get(url, headers: headers).parsed)
+      end
+
+      def headers
+        { "Accept" => "application/json" }
       end
     end
   end
--- omniauth-quickbooks-oauth2.gemspec
+++ omniauth-quickbooks-oauth2.gemspec
@@ -13,9 +13,8 @@ Gem::Specification.new do |spec|
   spec.homepage      = 'https://github.com/abeland/omniauth-quickbooks-oauth2'
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files`.split("\n")
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.files         = Dir["lib/**/*.rb"]
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'omniauth', '~>1.3'
